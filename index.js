a18cea3a558a33321fdaa17410b506b1432d1b2f
Ridokunda <ridokundanetshi@gmail.com>
2023-12-05 02:56:18 +0200

    Game function works

--- index.js
+++ index.js
@@ -7,27 +7,30 @@ function getComputerChoice(){
     return choice;
 }
 
+let obj = {
+    pscore : 0,
+    cscore : 0
+}
 
-
-function PlayRound(playerselection,computerselection,pscore,cscore){
+function PlayRound(playerselection,computerselection,obj){
     
     if(playerselection.toLowerCase() === "rock" && computerselection === "scissor"){
-        pscore = pscore + 1;
+        obj.pscore++;
         return "You win rock beats scissor!";
     }else if(playerselection.toLowerCase()=== "scissor" && computerselection === "rock"){
-        cscore = cscore + 1;
+        obj.cscore++;
         return "You lose rock beats scissor!";
     }else if(playerselection.toLowerCase() === "paper" && computerselection === "rock"){
-        pscore = pscore + 1;
+        obj.pscore++;
         return "You win paper beats rock!";
     }else if(playerselection.toLowerCase() === "rock" && computerselection === "paper"){
-        cscore = cscore + 1;
+        obj.cscore++;
         return "You lose paper beats rock!";
     }else if(playerselection.toLowerCase() === "scissor" && computerselection === "paper"){
-        pscore = pscore + 1;
+        obj.pscore++;
         return "You win scissor beats paper!";
     }else if(playerselection.toLowerCase() === "paper" && computerselection === "scissor"){
-        cscore = cscore + 1;
+        obj.cscore++;
         return "You lose scissor beats paper!";
     }else if(playerselection.toLowerCase() === computerselection){
         return "Draw. Try Again...";
@@ -38,26 +41,30 @@ function PlayRound(playerselection,computerselection,pscore,cscore){
 //const outcome = PlayRound(playerselection,computerselection);
 
 function game(){
-    let pscore = 0;
-    let cscore = 0;
+    let obj = {
+        pscore : 0,
+        cscore : 0
+    }
+    
     for(let i = 0; i < 5; i++){
         
         const playerselection = prompt("Choose rock, paper or scissor");
         const computerselection = getComputerChoice();
-        const outcome = PlayRound(playerselection,computerselection,pscore,cscore);
+        const outcome = PlayRound(playerselection,computerselection,obj);
         console.log(outcome);
-        console.log(pscore);
-        console.log(cscore);
-
-        if(pscore > cscore){
-            console.log("You won the game!"); 
-        }
-        if(cscore > pscore){
-            console.log("You lost the game...");
-        }
-        if(cscore = pscore){
-            console.log("Draw!!!");
-        }
+        console.log(obj.pscore);
+        console.log(obj.cscore);
+        
+       
+    }
+    if(obj.pscore > obj.cscore){
+        console.log("You won the game!"); 
+    }
+    if(obj.cscore > obj.pscore){
+        console.log("You lost the game...");
+    }
+    if(obj.cscore = obj.pscore){
+        console.log("Draw!!!");
     }
 }
 
